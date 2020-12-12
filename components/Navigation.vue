bbbd23147e8040f6ad258853187e8c9e5b77d842
Edmund Hee <edmund.hee05@gmail.com>
2020-12-12 11:01:29 +0800

    FS360 Online Day 1

---
+++ components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex py-6 px-4 font-poppins">
+    <nuxt-link
+      to="/"
+      class="text-white font-semibold ml-5 text-2xl hover:text-blue-500 transition-all duration-500 ease-in-out"
+      >EDMUND HEE</nuxt-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is the about page
+    <nuxt-link to="/" class="bg-blue-500 text-white py-4 px-2 ml-10"
+      >Home Page</nuxt-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
